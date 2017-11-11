2ccbb277ca3eac81f06b002674320310ad58e30b
Alexander Desesta <desesta@outlook.com>
2017-11-11 21:18:28 +0200

    Optimisation code

--- TelegramBot/ViewModels/BotViewModel.cs
+++ TelegramBot/ViewModels/BotViewModel.cs
@@ -92,102 +92,109 @@
 
 
 
-            //Орел и решка
-            String flipAnswer = ""; //для хранения ответа, орел решка
-            int Random = Rnd.Next(0, 2);
-            if (Random == 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (msg.Text.StartsWith("/start"))
             {
-                flipAnswer = "Орел";
+                Answer = "/flip - подбросить монетку\r\n/rand - случайное число от 1 до 47\r\n/scorebylitvinov - твоя оценка по Литвинову";
+            }
+            else if (msg.Text.StartsWith("/flip"))
+            {
+                //Орел и решка
+                String flipAnswer = ""; //для хранения ответа, орел решка
+                int Random = Rnd.Next(0, 2);
+                if (Random == 0)
+                {
+                    flipAnswer = "Орел";
+                }
+                else
+                {
+                    flipAnswer = "Решка";
+                }
+                Answer = flipAnswer;
+            }
+            else if(msg.Text.StartsWith("/rand"))
+            {
+                //Случайное число
+                int Rand = Rnd.Next(0, 48);
+                Answer = Rand.ToString();
+            }
+            else if(msg.Text.StartsWith("/scorebylitvinov"))
+            {           
+                //Узнай свою оценку по Литвинову
+                int RandLit = Rnd.Next(1, 11);
+                switch (RandLit)
+                {
+                    case 1:
+                        RandLit = Rnd.Next(1, 44);
+                        break;
+                    case 2:
+                        RandLit = Rnd.Next(10, 44);
+                        break;
+                    case 3:
+                        RandLit = Rnd.Next(10, 51);
+                        break;
+                    case 4:
+                        RandLit = Rnd.Next(15, 61);
+                        break;
+                    case 5:
+                        RandLit = Rnd.Next(15, 61);
+                        break;
+                    case 6:
+                        RandLit = Rnd.Next(15, 61);
+                        break;
+                    case 7:
+                        RandLit = Rnd.Next(15, 66);
+                        break;
+                    case 8:
+                        RandLit = Rnd.Next(20, 75);
+                        break;
+                    case 9:
+                        RandLit = Rnd.Next(50, 75);
+                        break;
+                    case 10:
+                        RandLit = Rnd.Next(65, 100);
+                        break;
+                }
+                if (RandLit < 60) { Answer = "Твоя оценка по Литвинову: " + RandLit.ToString();}
+                else { Answer = "Твоя оценка по Литвинову: " + RandLit.ToString() + ", гнида ебучая"; }
+
+            }
+            else if(msg.Text.StartsWith("/para"))
+            {
+                //Проверка на то, нужно ли идти на пару
+                int Random = Rnd.Next(0, 2);
+                String paraAnswer = ""; //для хранения ответа, нужно ли идти на пару
+                if (Random == 0)//Используем из Орла и решки, так как похожий принцип
+                {
+                    paraAnswer = "лучше сходить";
+                }
+                else
+                {
+                    paraAnswer = "можно не идти";
+                }
+                
+                Answer = "На следующую пару " + paraAnswer;
             }
             else
             {
-                flipAnswer = "Решка";
+                Answer = "Такой команды нет, так как Русик работает в Мейзу";
             }
 
-
-
-            //Случайное число
-            int Rand = Rnd.Next(0, 48);
-
-
-
-
-
-            //Узнай свою оценку по Литвинову
-            int RandLit = Rnd.Next(1, 11);
-            switch (RandLit)
-            {
-                case 1:
-                    RandLit = Rnd.Next(1, 44);
-                    break;
-                case 2:
-                    RandLit = Rnd.Next(10, 44);
-                    break;
-                case 3:
-                    RandLit = Rnd.Next(10, 51);
-                    break;
-                case 4:
-                    RandLit = Rnd.Next(15, 61);
-                    break;
-                case 5:
-                    RandLit = Rnd.Next(15, 61);
-                    break;
-                case 6:
-                    RandLit = Rnd.Next(15, 61);
-                    break;
-                case 7:
-                    RandLit = Rnd.Next(15, 66);
-                    break;
-                case 8:
-                    RandLit = Rnd.Next(20, 75);
-                    break;
-                case 9:
-                    RandLit = Rnd.Next(50, 75);
-                    break;
-                case 10:
-                    RandLit = Rnd.Next(65, 100);
-                    break;
-            }
-
-
-            //Проверка на то, нужно ли идти на пару
-            String paraAnswer = ""; //для хранения ответа, нужно ли идти на пару
-            if (Random == 0)//Используем из Орла и решки, так как похожий принцип
-            {
-                paraAnswer = "лучше сходить";
-            }
-            else
-            {
-                paraAnswer = "можно не идти";
-            }
-
-
-
-            switch (msg.Text)
-            {
-                case "/start": Answer = "/flip - подбросить монетку\r\n/rand - случайное число от 1 до 47\r\n/scorebylitvinov - твоя оценка по Литвинову"; break;
-                case "/flip": Answer = flipAnswer; break;
-                case "/flip@Squad47bot": Answer = flipAnswer; break;
-
-                case "/rand": Answer = Rand.ToString(); break;
-                case "/rand@Squad47bot": Answer = Rand.ToString(); break;
-
-                case "/scorebylitvinov": Answer = "Твоя оценка по Литвинову: " + RandLit.ToString(); break;
-                case "/scorebylitvinov@Squad47bot": Answer = "Твоя оценка по Литвинову: " + RandLit.ToString(); break;
-
-                case "/para": Answer = "На следующую пару " + paraAnswer; break;
-                case "/para@Squad47bot": Answer = "На следующую пару " + paraAnswer; break;
-
-
-                default: Answer = "Такой команды нет, так как Русик работает в Мейзу"; break;
-            }
-
-            /*await Dispatcher.BeginInvoke(new ThreadStart(delegate {
-                LogTextBlock.Text += "\r\n" + "Bot received command: " + msg.Text;
-                LogTextBlock.Focus();
-                LogTextBlock.CaretIndex = LogTextBlock.Text.Length;
-                LogTextBlock.ScrollToEnd();
-            }));*/
 
             await Bot.SendTextMessageAsync(msg.Chat.Id, Answer);
 
