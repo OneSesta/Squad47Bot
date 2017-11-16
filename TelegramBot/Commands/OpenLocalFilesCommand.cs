72337c473a5957caf0cef4381e7427076518560c
Alexander Desesta <sestakiller@gmail.com>
2017-11-16 08:28:06 +0200

    Added button for opening application directory. The application received an icon

---
+++ TelegramBot/Commands/OpenLocalFilesCommand.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBot.Commands
+{
+    using System;
+    using System.Diagnostics;
+    using System.Windows.Input;
+    using TelegramBot.ViewModels;
+
+    /// <summary>
+    /// This command is responsible for opening folder with documents.
+    /// </summary>
+    class OpenLocalFilesCommand : ICommand
+    {
+        private MainWindowViewModel _viewModel;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="viewModel">Target ViewModel</param>
+        public OpenLocalFilesCommand(MainWindowViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        #region ICommand implementation
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Process.Start(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location));
+        }
+        #endregion
+    }
+}
