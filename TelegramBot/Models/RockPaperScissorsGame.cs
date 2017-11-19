2050105652082f62619b8e7a75f6144559b42c55
Vladislav Gooba <vladhuba@ukr.net>
2017-11-19 14:12:12 +0200

    Preparations for inline buttons

--- TelegramBot/Models/RockPaperScissorsGame.cs
+++ TelegramBot/Models/RockPaperScissorsGame.cs
@@ -8,14 +8,26 @@
     using Telegram.Bot;
     using Telegram.Bot.Types;
     using Telegram.Bot.Types.Enums;
+    using Telegram.Bot.Types.InlineKeyboardButtons;
+    using Telegram.Bot.Types.ReplyMarkups;
 
     class RockPaperScissorsGame : IBotCommandHandler
     {
         private TelegramBotClient _client;
 
+        //private class Game
+        //{
+        //    public Message gameMessage;
+        //    public User player1;
+        //    public User player2;
+        //}
+
+        //private List<Game> currentGames;
+
         public RockPaperScissorsGame(TelegramBotClient client)
         {
             _client = client;
+            //currentGames = new List<Game>(10);
         }
 
         public void HandleUpdate(Update update)
@@ -24,23 +36,33 @@
             string request = Utils.PrettifyCommand(update.Message.Text);
             string answer;
 
-            //if someone just wants to start a game
-            if (request == "/rockpaperscissors")
-            {
-                answer = "Камень, ножницы, бумага:\r\n" +
-                    "/scissors - Выбрать ножницы (\U0000270C)\r\n" +
-                    "/paper - Выбрать бумагу (\U0000270B)\r\n" +
-                    "/rock - Выбрать камень (\U0000270A)\r\n";
-                _client.SendTextMessageAsync(update.Message.Chat.Id, answer, ParseMode.Default, false, false, update.Message.MessageId);
-                return;
-            }
-
             string[] variants = new string[] { "/scissors", "/paper", "/rock" };
 
             //\U0000270A - Камень
             //\U0000270B - Бумага
             //\U0000270C - Ножницы
             string[] presentations = new string[] { "\U0000270C", "\U0000270B", "\U0000270A" };
+
+
+            //if someone just wants to start a game
+            if (request == "/rockpaperscissors")
+            {
+                //var keyboard = new InlineKeyboardMarkup(new InlineKeyboardButton[][]
+                //{
+                //    new InlineKeyboardButton[]
+                //    {
+                //        new InlineKeyboardCallbackButton(presentations[0], variants[0]),
+                //        new InlineKeyboardCallbackButton(presentations[1], variants[1]),
+                //        new InlineKeyboardCallbackButton(presentations[2], variants[2])
+                //    }
+                //});
+                answer = "Камень, ножницы, бумага:\r\n" +
+                    "/scissors - Выбрать ножницы (\U0000270C)\r\n" +
+                    "/paper - Выбрать бумагу (\U0000270B)\r\n" +
+                    "/rock - Выбрать камень (\U0000270A)\r\n";
+                _client.SendTextMessageAsync(update.Message.Chat.Id, answer, ParseMode.Default, false, false, update.Message.MessageId/*, keyboard*/);
+                return;
+            }
 
             int playerAnswer = Array.IndexOf(variants, request);
             int botAnswer = (new Random()).Next(3);
@@ -62,21 +84,35 @@
             answer = $"{presentations[playerAnswer]} vs {presentations[botAnswer]}\r\n{outcome}";
 
             _client.SendTextMessageAsync(update.Message.Chat.Id, answer, ParseMode.Default, false, false, update.Message.MessageId);
+
+
         }
 
         public bool CanHandleUpdate(Update update)
         {
-            if (update.Type != UpdateType.MessageUpdate)
-                return false;
 
-            string request = Utils.PrettifyCommand(update.Message.Text);
+            //if ((update.Type == UpdateType.CallbackQueryUpdate)
+            //    && currentGames.FindAll(
+            //        g => 
+            //        (g.gameMessage.Chat.Id == update.Message.Chat.Id
+            //        && g.gameMessage.MessageId == update.Message.MessageId))
+            //    .Count == 1)
+            //{
+            //    return true;
+            //}
 
-            return request == "/rock"
-                || request == "/scissors"
-                || request == "/paper"
-                || request == "/rockpaperscissors";
+            if (update.Type == UpdateType.MessageUpdate)
+            {
+                string request = Utils.PrettifyCommand(update.Message.Text);
+
+                return request == "/rock"
+                    || request == "/scissors"
+                    || request == "/paper"
+                    || request == "/rockpaperscissors";
+            }
+            return false;
         }
-        
+
 
     }
 }
