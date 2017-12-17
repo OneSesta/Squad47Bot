966063214054eb2c5c4baf0c8777cc65a49ae7e7
Vladislav Gooba <vladhuba@ukr.net>
2017-12-17 15:36:19 +0200

    FilesAccessor module

---
+++ FilesAccessorModule/FilesAccessorModule.cs
@@ -0,0 +1,35 @@
+﻿using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TelegramBot.Common;
+using Unity;
+using Unity.Attributes;
+
+namespace FilesAccessorModule
+{
+    [Module(ModuleName = "FilesAccessorModule")]
+    [ModuleDependency("BotUpdateDispatcherModule")]
+
+    public class FilesAccessorModule : IModule
+    {
+        IUnityContainer _unityContainer;
+        IBotUpdateDispatcher _updateDispatcher;
+        IBotLogger _logger;
+
+        public FilesAccessorModule(IUnityContainer unityContainer, IBotUpdateDispatcher updateDispatcher, [OptionalDependency] IBotLogger logger)
+        {
+            _unityContainer = unityContainer;
+            _updateDispatcher = updateDispatcher;
+            _logger = logger;
+        }
+
+        public void Initialize()
+        {
+            _unityContainer.RegisterInstance<IBotUpdateHandler>("FilesAccessorModule", new FilesAccessor(_updateDispatcher, _logger));
+        }
+    }
+}
